407b37a24e2e83f3dcf18604728f753c4b3dc69b
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-02-28 14:34:46 +0200

    Add ontouchstart to check from phone

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -4,12 +4,15 @@ const gElGallery = document.querySelector('.gallery')
 
 function renderGallery() {
     var strHTML = ''
-    strHTML += `<img src="img/1.jpg" id="img1" onclick="onSelectImg(this)">
-     <img src="img/2.jpg" id="img2" onclick="onSelectImg(this)">`
+    strHTML += `<img src="img/1.jpg" id="img1" ontouchstart="onSelectImg(this)" onclick="onSelectImg(this)">
+     <img src="img/2.jpg" id="img2" ontouchstart="onSelectImg(this)" onclick="onSelectImg(this)">`
     gElGallery.innerHTML = strHTML
 }
 
 function onSelectImg(elImg) {
+    const elSecMemeEditor = document.querySelector('.meme-editor')
+    elSecMemeEditor.hidden = false
+    gElGallery.hidden = true
     setImg(elImg)
     renderMeme()
 }
