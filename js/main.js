54690261dd92f9808436cf9cc35f65d6b1dd828e
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-02-28 14:55:51 +0200

    Add support to touch events

--- js/main.js
+++ js/main.js
@@ -2,4 +2,26 @@
 
 function onInit() {
     renderGallery()
+    const images = document.querySelectorAll('img')
+    images.forEach(img => {
+        addImgListeners(img)
+    })
+}
+
+function addImgListeners(img) {
+    addImgMouseListeners(img)
+    addImgTouchListeners(img)
+    
+}
+
+function addImgMouseListeners(img) {
+    img.addEventListener('click', onSelectImg)
+    // img.addEventListener('mousemove', onMove)
+    // gElCanvas.addEventListener('mouseup', onUp)
+}
+
+function addImgTouchListeners(img) {
+    img.addEventListener('touchstart', onSelectImg)
+    // gElCanvas.addEventListener('touchmove', onMove)
+    // gElCanvas.addEventListener('touchend', onUp)
 }
