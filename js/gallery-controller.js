a227476147729f566841ae8d4029ed41fabc8b68
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-04 00:01:11 +0200

    Add filter functions by input and buttons

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -1,33 +1,41 @@
 'use strict'
-var gImgs
+
 const keywordsDB = 'keywordsDB'
-var keywordsInit = {
-    angry: 0,
-    cute: 0,
-    political: 0,
-    animals: 0,
-    baby:0,
-    famous: 0,
-    israeli: 0,
-    television: 0,
-    funny: 0,
-    man: 0
-}
 
 const elSearchKeywords = document.querySelector('.search-keywords')
 const gElGallery = document.querySelector('.gallery')
 const elSecMemeEditor = document.querySelector('.meme-editor')
+const elSearchInput = document.querySelector('.search-pic-input')
 
 function renderGallery() {
-    const images = getImages()
-    var strHTML = ''
+    let images = getImgs()
+    if(!images) {
+        setImages()
+        images = getImgs()
+    }
+    renderKeywords()
+    let strHTML = ''
     strHTML = images.map(img => {
         return `<img src="${img.url}" id="img${img.id}">`
     }).join('')
-    gElGallery.innerHTML = strHTML    
+    gElGallery.innerHTML = strHTML
     addImgListeners()
     if (gElGallery.style.display === 'none') gElGallery.style.display = 'grid'
     if (elSecMemeEditor.style.display != 'none') elSecMemeEditor.style.display = 'none'
+}
+
+function renderKeywords() {
+    const elSearchKeywords = document.querySelector('.search-keywords')
+    let strHTML = ''
+    let keywords = loadFromStorage(keywordsDB)
+    if (!keywords) {
+        keywords = getKeywords()
+        saveToStorage(keywordsDB, keywords)
+    }
+    for(let keyword in keywords) {
+        strHTML += `<li><button class="btn ${keyword} keyword-btn" onclick="onKeywordBtnClick(event)">${keyword}</button></li>`
+    }
+    elSearchKeywords.innerHTML = strHTML
 }
 
 function onSelectImg(ev) {
@@ -37,4 +45,20 @@ function onSelectImg(ev) {
     setImg(ev.target)
     setBorderSize()
     renderMeme()
+}
+
+function onKeywordBtnClick(ev) {
+    ev.stopPropagation()
+    filterImgs(ev.target.innerText)
+    updateKeyword(ev.target.innerText)
+    setKeywordSize(ev.target.innerText, ev.target)
+    renderGallery()
+    saveToStorage(keywordsDB, keywords)
+}
+
+function onKeywordInput(ev) {
+    ev.stopPropagation()
+    filterImgs(ev.target.value)
+    updateKeyword(ev.target.value)
+    renderGallery()
 }
--- js/services/gallery-service.js
+++ js/services/gallery-service.js
@@ -1,5 +1,19 @@
 'use strict'
 
+var gImgs
+var keywords = {
+    angry: 0,
+    cute: 0,
+    political: 0,
+    animals: 0,
+    baby:0,
+    famous: 0,
+    israeli: 0,
+    television: 0,
+    funny: 0,
+    man: 0
+}
+
 function setImg(elImg) {
     const elImgNumId = elImg.id.substring(3)
     gMeme.selectedImgId = +elImgNumId
@@ -26,7 +40,7 @@ function setBorderSize() {
     elBorder.style.width = (gElCanvas.width - 100) + 'px'
 }
 
-function getImages() {
+function setImages() {
     gImgs = []
     gImgs.push({id: 1,url: `img/1.jpg`, keywords: ['angry', 'political']})
     gImgs.push({id: 2,url: `img/2.jpg`, keywords: ['animals', 'cute']})
@@ -48,4 +62,30 @@ function getImages() {
     gImgs.push({id: 18,url: `img/18.jpg`, keywords: ['television', 'cute', 'famous']})
     gImgs.push({id: 19,url: `img/19.jpg`, keywords: ['television', 'man']})
     return gImgs
+}
+
+function getKeywords() {
+    return keywords
+}
+
+function filterImgs(keyword) {
+    const regex = new RegExp(keyword, 'i')
+    gImgs = setImages() 
+    gImgs = gImgs.filter(img => {
+        return img.keywords.some(keyword => regex.test(keyword))
+    })
+}
+
+function getImgs() {
+    return gImgs
+}
+
+function updateKeyword(keyword) {
+    elSearchInput.value = keyword
+    keywords[keyword]++
+}
+
+function setKeywordSize(keyword, keywordBtn) {
+    keywordBtn.style.fontSize = `${keywords[keyword] + 1}em`
+    console.log(keywordBtn.style.fontSize,`${keywords[keyword]}` )
 }
